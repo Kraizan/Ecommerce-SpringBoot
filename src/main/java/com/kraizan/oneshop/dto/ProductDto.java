83132a81700715d5063cffe47bb78f5ca4d98402
Vanshaj Bhatnagar <vanshajbhatnagar10@gmail.com>
2024-11-12 18:08:39 +0530

    added UserController with CRUD endpoints for user management; modified ProductDto to include description field

--- src/main/java/com/kraizan/oneshop/dto/ProductDto.java
+++ src/main/java/com/kraizan/oneshop/dto/ProductDto.java
@@ -12,9 +12,9 @@ public class ProductDto {
     private Long id;
     private String name;
     private String brand;
-    private String description;
     private BigDecimal price;
     private int inventory;
+    private String description;
     private Category category;
     private List<ImageDto> images;
 }
