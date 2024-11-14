72cd1871db0c7be761e5ba84637615df5f8aabb5
Vanshaj Bhatnagar <vanshajbhatnagar10@gmail.com>
2024-11-14 22:31:38 +0530

    feat: implement user authentication; add ShopUserDetails and ShopUserDetailsService for user details management; update UserRepository with findByEmail method; added  JWT dependencies

--- src/main/java/com/kraizan/oneshop/repository/UserRepository.java
+++ src/main/java/com/kraizan/oneshop/repository/UserRepository.java
@@ -6,4 +6,5 @@ import com.kraizan.oneshop.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
+    User findByEmail(String email);
 }
--- src/main/java/com/kraizan/oneshop/security/user/ShopUserDetailsService.java
+++ src/main/java/com/kraizan/oneshop/security/user/ShopUserDetailsService.java
@@ -1,14 +1,27 @@
 package com.kraizan.oneshop.security.user;
 
+import java.util.Optional;
+
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+import com.kraizan.oneshop.model.User;
+import com.kraizan.oneshop.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class ShopUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = Optional.ofNullable(userRepository.findByEmail(email))
+                    .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+        return ShopUserDetails.buildUserDetails(user);
     }
 
 }
